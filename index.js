272243abea2297b0924b58a9d6889a78bec7d0a4
sid3github <siddharthpadwal3@gmail.com>
2018-08-07 00:05:19 +0530

    static server / serve index

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+// var serveStatic = require('serve-static');
+const serveIndex = require('serve-index');
 
 
 const server = express();
+
+server.use('/ftp', express.static('views/ftp'), serveIndex('views/ftp', {'icons': true}));
+
+// server.use(serveStatic('views/ftp', {'index': ['index.html', 'index.htm']}));
 
 // static route.
 server.use(express.static('views'));
 
 server.use(bodyParser.json());
 
-server.get('/static',function(req,res){
+server.get('/ftp',function(req,res){
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
